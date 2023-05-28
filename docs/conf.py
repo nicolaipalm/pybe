9fd4856c7ec943e2b96a7c340b9c3b8ec971539e
Nico Palm <nicolaipalm@googlemail.com>
2023-05-28 14:43:41 +0200

    adapt sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
 autoclass_content = 'both'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,6 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
+html_theme_path = ['_themes', ]
 
 html_theme_options = {
     'github_url': 'https://github.com/nicolaipalm/pybe',
@@ -61,4 +62,4 @@ html_title = 'PyBe'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
